3bfa4545e1dfb35f66141329f867f64351a3bd30
Astromub Limited <paanobaga@outlook.com>
2025-11-04 10:44:39 +0800

    Add files via upload

---
+++ license_list.js
@@ -0,0 +1,11 @@
+/**
+ * LICENSE_LIST.JS
+ * Contains the license keys for tier validation.
+ * In a production environment, this data should be fetched securely 
+ * from a server-side API, not stored in a public JS file.
+ */
+const licenseKeys = {
+    'PRO': 'PRO-ASTRON-2025',
+    'PRO': 'PRO-ASTRON-2024',
+    'UNLIMITED': 'UNLIMITED-ASTRON-2025'
+};
